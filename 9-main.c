60ece3dcb0e544c479d208dd56cc57a5a164709f
Segun Adeleke <oadeleke123@gmail.com>
2023-08-31 14:11:20 +0100

    Add: get height of tree

---
+++ 9-main.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    binary_tree_t *root;
+    size_t height;
+
+    root = binary_tree_node(NULL, 98);
+    root->left = binary_tree_node(root, 12);
+    root->right = binary_tree_node(root, 402);
+    binary_tree_insert_right(root->left, 54);
+    binary_tree_insert_right(root->right, 55);
+    binary_tree_insert_left(root->left, 200);
+    binary_tree_insert_left(root->left, 201);
+    binary_tree_insert_left(root->left, 202);
+    binary_tree_insert_left(root->left, 203);
+    binary_tree_insert_left(root->left, 204);
+    binary_tree_insert_right(root->left, 56);
+    binary_tree_insert_right(root, 128);
+    binary_tree_print(root);
+
+    height = binary_tree_height(root);
+    printf("Height from %d: %lu\n", root->n, height);
+    height = binary_tree_height(root->right);
+    printf("Height from %d: %lu\n", root->right->n, height);
+    height = binary_tree_height(root->left->right);
+    printf("Height from %d: %lu\n", root->left->right->n, height);
+    height = binary_tree_height(root->left->left->left);
+    printf("Height from %d: %lu\n", root->left->left->left->n, height);
+    return (0);
+}
